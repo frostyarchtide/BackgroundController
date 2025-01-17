66c93aec8d23ae82f937d447b86e23e293f8414f
Frosty <138848995+frostyarchtide@users.noreply.github.com>
2025-01-16 20:49:36 -0500

    Initial commit

---
+++ BackgroundController.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using MSCLoader;
+using UnityEngine;
+
+namespace BackgroundController
+{
+    public class BackgroundController : Mod
+    {
+        public override string ID => "BackgroundController";
+        public override string Name => "Background Controller";
+        public override string Author => "Frosty";
+        public override string Version => "1.0";
+        public override string Description => "Control whether the game runs in the background or not.";
+
+        public static Keybind ToggleRunInBackground { get; set; }
+
+        public static SettingsCheckBox RunInBackground { get; set; }
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.ModSettings, Mod_Settings);
+            SetupFunction(Setup.ModSettingsLoaded, Mod_ModSettingsLoaded);
+            SetupFunction(Setup.Update, Mod_Update);
+        }
+
+        private void Mod_Settings()
+        {
+            ToggleRunInBackground = Keybind.Add(this, "toggleRunInBackground", "Toggle Run in Background", KeyCode.RightControl);
+
+            RunInBackground = Settings.AddCheckBox("runInBackground", "Run in Background", false, () => SetRunInBackground());
+        }
+
+        private void Mod_ModSettingsLoaded()
+        {
+            SetRunInBackground();
+        }
+
+        private void Mod_Update()
+        {
+            if (ToggleRunInBackground.GetKeybindDown())
+            {
+                bool value = RunInBackground.GetValue();
+                RunInBackground.SetValue(!value);
+                SetRunInBackground();
+                ModConsole.Print("Run in background " + (value ? "disabled" : "enabled") + ".");
+            }
+        }
+
+        private void SetRunInBackground()
+        {
+            Application.runInBackground = RunInBackground.GetValue();
+        }
+    }
+}
